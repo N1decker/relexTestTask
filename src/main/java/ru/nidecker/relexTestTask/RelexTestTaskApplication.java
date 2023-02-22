4391b6c648da74b2696c7c823dbc3b1fe17cfffd
Egor Ershov <eg0r.ershoff@yandex.ru>
2023-02-22 10:20:14 +0300

    creating project

---
+++ src/main/java/ru/nidecker/relexTestTask/RelexTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.nidecker.relexTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelexTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelexTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nidecker/relexTestTask/RelexTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nidecker.relexTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelexTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
