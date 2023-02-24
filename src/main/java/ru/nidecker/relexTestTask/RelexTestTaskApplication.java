11119c668f53900488682ee13bcf21a5a5ad529d
Egor Ershov <eg0r.ershoff@yandex.ru>
2023-02-24 15:31:46 +0300

    now that the app has started, swagger-ui opens in Chrome

--- src/main/java/ru/nidecker/relexTestTask/RelexTestTaskApplication.java
+++ src/main/java/ru/nidecker/relexTestTask/RelexTestTaskApplication.java
@@ -3,11 +3,18 @@ package ru.nidecker.relexTestTask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class RelexTestTaskApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(RelexTestTaskApplication.class, args);
+		openSwaggerUI();
 	}
 
+	private static void openSwaggerUI() throws IOException {
+		Runtime rt = Runtime.getRuntime();
+		rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/swagger-ui.html");
+	}
 }
